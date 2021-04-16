2b19baeea137ac4f835916888726c350f3d119c8
Andre Rosa <ar@gsmk.de>
2021-04-16 12:38:56 +0200

    🍻 Add log on prisma shutdown

--- src/client.ts
+++ src/client.ts
@@ -2,4 +2,8 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+prisma.$on('beforeExit', async () => {
+  console.log('👋🏼 Shutting down Prisma server')
+})
+
 export default prisma
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import helmet from 'helmet'
 import prisma from './client'
 import authRoutes from './routes/auth'
-import rootstockRoutes from './routes/rootstock'
+import rootstockRoutes from 'routes/rootstock'
 import bcrypt from 'bcryptjs'
 
 const PORT = process.env.PORT || 8080
@@ -22,7 +22,7 @@ app.use(rootstockRoutes)
 app.listen(PORT, async () => {
   console.log(`Running on ${PORT} ⚡`)
 
-  const user = await prisma.user.findFirst({ where: { email: process.env.ROOT_EMAIL } })
+  const user = await prisma.user.findUnique({ where: { email: process.env.ROOT_EMAIL } })
 
   if (!user) {
     const hashedPw = await bcrypt.hash(process.env.ROOT_PASS + '', 12)
